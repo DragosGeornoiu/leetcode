c8462300d666d48a5a596324886a415a9de34302
dragos.geornoiu <dragos.geornoiu@gmail.com>
2022-12-30 21:45:02 +0200

    Formatted code.

--- src/com/dragos/geornoiu/arrays/sorted/TwoSum.java
+++ src/com/dragos/geornoiu/arrays/sorted/TwoSum.java
@@ -5,9 +5,13 @@ public class TwoSum {
      * 167. Two Sum II - Input Array Is Sorted
      * https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
      *
-     * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
      * <p>
-     * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+     * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers
+     * such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2]
+     * where 1 <= index1 < index2 <= numbers.length.
+     * <p>
+     * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of
+     * length 2.
      * <p>
      * The tests are generated such that there is exactly one solution. You may not use the same element twice.
      * <p>
@@ -101,6 +105,7 @@ public class TwoSum {
                 return new int[]{leftPointer + 1, rightPointer + 1};
             }
         }
+
         return new int[]{-1, -1};
     }
 
